66672cd50ef9feb16a86f5a74f1e01bf27e13c68
Anulfo <noaharkling@gmail.com>
2016-10-18 13:35:56 -0500

    Trying to modify LineItem

---
+++ Controllers/LineItem.cs
@@ -0,0 +1,18 @@
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Mvc;
+// using Bangazon.Data;
+// using Bangazon.Models;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.AspNetCore.Http;
+
+// namespace Bangazon.Controllers
+// {
+//     [produces("aplication/json")]
+//     [route("[controller]")]
+
+//     public class LineItemController  controller
+    
+// }
